c5f93c21767cc4a5819c071bd82a3b12bb47be22
barophobe <edoconnolly@gmail.com>
2017-11-10 21:05:38 +0000

    first commit

---
+++ assets/app/messages/message-input.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+import { MessageService } from "./message.service";
+import { Message } from "./message.model";
+
+@Component({
+	selector: 'app-message-input',
+	templateUrl: './message-input.component.html'
+	
+})
+
+export class MessageInputComponent {
+	constructor(private messageService: MessageService) {}
+
+	onSave(value: string) {
+		const message = new Message(value, 'Ed');
+		this.messageService.addMessage(message);
+	}
+
+}
